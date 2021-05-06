53f1aa741ef4b0576207264b8d0be12aa1b9e411
NaHyeon <nahyeonee99@gmail.com>
2021-05-06 16:06:09 +0900

    -#17 Templates end

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,31 @@
-const fakeUser = {
-  userName: "Nahyeon",
-  loggedIn: true,
-};
-
 export const home = (req, res) => {
-  res.render("home", { pageTitle: "Home", fakeUser });
+  const videos = [
+    {
+      title: "video #1",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minute ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "video #2",
+      rating: 5,
+      comments: 3,
+      createdAt: "2 minute ago",
+      views: 59,
+      id: 2,
+    },
+    {
+      title: "video #3",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minute ago",
+      views: 59,
+      id: 3,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
 };
 export const search = (req, res) => {
   res.render("search");
