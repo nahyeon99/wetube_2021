f656974c3dbd1e6fecf223bdac1aa1d59c155305
NaHyeon <nahyeonee99@gmail.com>
2021-05-17 16:46:32 +0900

    #18.8 Connecting to Mongo

---
+++ src/db.js
@@ -0,0 +1,14 @@
+import mongoose, { connect } from "mongoose";
+
+mongoose.connect("mongodb://127.0.0.1:27017/wetube_2021", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅  Connected to DB");
+
+db.on("error", (error) => console.log("❌  DB Error", error));
+
+db.once("open", handleOpen);
