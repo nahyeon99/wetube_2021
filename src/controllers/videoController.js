c4b68f8f2753ae8cf15ae17911ad007e9da9dfea
NaHyeon <nahyeonee99@gmail.com>
2021-05-17 16:24:07 +0900

    #18.6 More Practice part Two

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -68,6 +68,17 @@ export const getUpload = (req, res) => {
 };
 
 export const postUpload = (req, res) => {
-  // here we will add a video to the videos array.
+  const {
+    body: { title },
+  } = req;
+  const newVideo = {
+    title,
+    rating: 0,
+    comments: 0,
+    createdAt: "Just Now",
+    views: 0,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
   return res.redirect("/");
 };
