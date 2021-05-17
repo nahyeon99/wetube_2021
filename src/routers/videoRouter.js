77c5264d13099dfc867375a2828c1ee41b5f3ef0
NaHyeon <nahyeonee99@gmail.com>
2021-05-17 16:17:03 +0900

    #18.5 More Practice part one

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,11 +3,14 @@ import {
   watch,
   postEditVideo,
   getEditVideo,
+  getUpload,
+  postUpload,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEditVideo).post(postEditVideo);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
